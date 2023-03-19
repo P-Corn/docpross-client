28ea227deae51d3ce09b1a8efac68380cefba54f
Peyton Cornelison <pcornelison@inceptionllc.com>
2023-03-19 12:24:01 -0500

    First commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+// Peyton: Here's what this document is for https://nextjs.org/docs/advanced-features/custom-document
 
 export default function Document() {
   return (
